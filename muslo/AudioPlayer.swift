a9f9d648e9d127f05bbababeef69329a43c5965c
ShaderLaze <sasha-galperov@yandex.ru>
2025-06-24 23:52:02 +0600

    Handle security scoped resource when playing audio

--- muslo/AudioPlayer.swift
+++ muslo/AudioPlayer.swift
@@ -5,6 +5,13 @@ final class AudioPlayer: ObservableObject {
     private var player: AVAudioPlayer?
 
     func play(url: URL) {
+        let needsStop = url.startAccessingSecurityScopedResource()
+        defer {
+            if needsStop {
+                url.stopAccessingSecurityScopedResource()
+            }
+        }
+
         do {
             player = try AVAudioPlayer(contentsOf: url)
             player?.play()
