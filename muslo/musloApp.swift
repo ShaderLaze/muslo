ce12265b98216168042b3c8c5595025b96ad25e7
ShaderLaze <sasha-galperov@yandex.ru>
2025-06-24 23:18:53 +0600

    Initial Commit

---
+++ muslo/musloApp.swift
@@ -0,0 +1,17 @@
+//
+//  musloApp.swift
+//  muslo
+//
+//  Created by Павел Соколов on 24.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct musloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musloTests/musloTests.swift
@@ -0,0 +1,17 @@
+//
+//  musloTests.swift
+//  musloTests
+//
+//  Created by Павел Соколов on 24.06.2025.
+//
+
+import Testing
+@testable import muslo
+
+struct musloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
